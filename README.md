97bccfea14279b52a0eaffeb21da67063a24fddd
unknown <unknown@example.com>
2020-07-19 13:32:56 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# 不知道要干嘛的小程序
+  慢慢玩吧
+
+## 参考文档
+
+- [云开发文档](https://developers.weixin.qq.com/miniprogram/dev/wxcloud/basis/getting-started.html)
+
---
+++ miniprogram/pages/index/index.js
@@ -0,0 +1,177 @@
+//index.js
+const app = getApp()
+
+//连接云数据库
+const cloud_db = wx.cloud.database({
+  //指定云数据库环境，填写环境ID而非环境名称
+  env:"test-6hxur"    
+  //env:"produce"
+});
+
+Page({
+  data: {
+    avatarUrl: './user-unlogin.png',
+    userInfo: {},
+    logged: false,
+    takeSession: false,
+    requestResult: '',
+
+    message: "XXX, 你好！",
+  },
+
+  ClickLeft: function() {
+    this.setData ({
+      message: "达瓦里氏，你好！"
+    })
+  },
+
+  ClickRight: function() {
+    this.setData({
+      message: "肮脏的资本家，你爬！"
+    })
+  },
+
+  formsubmit:function(e){
+    const that = this;
+    //上传数据
+    cloud_db.collection('test').add({  // 集合名称
+      data:{
+        name: e.detail.value.cloud_user_name,
+        age: e.detail.value.cloud_user_age
+      }
+    }).then(res=>{
+      console.log(res);
+    }).catch(err=>{
+      console.log(err);
+    })
+    //更新数据
+    this.showInfo();
+  
+  },
+
+  /*
+  从数据库test获取已有信息，并存储在cloud_data里
+  */
+  showInfo: function(){
+    cloud_db.collection('test').get({
+      success: res =>{
+        console.log(res.data)
+        this.setData({
+          cloud_data: res.data
+        })
+      }
+    })
+  },
+
+  onLoad: function() {
+    var _this = this;
+    if (!wx.cloud) {
+      wx.redirectTo({
+        url: '../chooseLib/chooseLib',
+      })
+      return
+    }
+
+    //展示数据库信息
+    this.showInfo();
+
+    // 获取用户信息
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          wx.getUserInfo({
+            success: res => {
+              this.setData({
+                avatarUrl: res.userInfo.avatarUrl,
+                userInfo: res.userInfo
+              })
+            }
+          })
+        }
+      }
+    })
+  },
+
+  onGetUserInfo: function(e) {
+    if (!this.data.logged && e.detail.userInfo) {
+      this.setData({
+        logged: true,
+        avatarUrl: e.detail.userInfo.avatarUrl,
+        userInfo: e.detail.userInfo
+      })
+    }
+  },
+
+  onGetOpenid: function() {
+    // 调用云函数
+    wx.cloud.callFunction({
+      name: 'login',
+      data: {},
+      success: res => {
+        console.log('[云函数] [login] user openid: ', res.result.openid)
+        app.globalData.openid = res.result.openid
+        wx.navigateTo({
+          url: '../userConsole/userConsole',
+        })
+      },
+      fail: err => {
+        console.error('[云函数] [login] 调用失败', err)
+        wx.navigateTo({
+          url: '../deployFunctions/deployFunctions',
+        })
+      }
+    })
+  },
+
+  // 上传图片
+  doUpload: function () {
+    // 选择图片
+    wx.chooseImage({
+      count: 1,
+      sizeType: ['compressed'],
+      sourceType: ['album', 'camera'],
+      success: function (res) {
+
+        wx.showLoading({
+          title: '上传中',
+        })
+
+        const filePath = res.tempFilePaths[0]
+        
+        // 上传图片
+        const cloudPath = 'my-image' + filePath.match(/\.[^.]+?$/)[0]
+        wx.cloud.uploadFile({
+          cloudPath,
+          filePath,
+          success: res => {
+            console.log('[上传文件] 成功：', res)
+
+            app.globalData.fileID = res.fileID
+            app.globalData.cloudPath = cloudPath
+            app.globalData.imagePath = filePath
+            
+            wx.navigateTo({
+              url: '../storageConsole/storageConsole'
+            })
+          },
+          fail: e => {
+            console.error('[上传文件] 失败：', e)
+            wx.showToast({
+              icon: 'none',
+              title: '上传失败',
+            })
+          },
+          complete: () => {
+            wx.hideLoading()
+          }
+        })
+
+      },
+      fail: e => {
+        console.error(e)
+      }
+    })
+  },
+
+})
